7d462a91f3325c5db48c4149e261e1cf5ffec6ba
Nikhil <nikhilpal2017@gmail.com>
2022-11-08 23:56:01 +0530

    [Initial Commit]

---
+++ src/components/NewsItem.js
@@ -0,0 +1,22 @@
+import { getByTitle } from '@testing-library/react'
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let {title, description} = this.props;
+        return (
+            <div>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/FDF6/production/_127541056_mitchell.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                            <a href="/newsDetail" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
